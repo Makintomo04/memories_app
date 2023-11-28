5aea428a0e91abf1235de6d2afee6e14553631ea
Michael Akintomo <1721809@alumni.brunel.ac.uk>
2023-11-28 23:57:27 +0000

    first commit

--- server/index.js
+++ server/index.js
@@ -14,11 +14,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://makintomo4:bergkamp10@mycluster.dmzr7vr.mongodb.net/posts';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} did not connect!`));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
